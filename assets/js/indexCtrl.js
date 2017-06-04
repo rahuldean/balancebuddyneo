8692f8adc62ba03f99a57defd015d798bb75b5df
Rahul Reddy <rahuldean@gmail.com>
2017-06-04 13:00:47 +0530

    showing codes in ui

--- assets/js/indexCtrl.js
+++ assets/js/indexCtrl.js
@@ -1,9 +1,13 @@
 angular.module('bbApp')
 .controller('indexCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.message = "index"
+    $scope.data = []
+
     $http.get('assets/raw/data.json')
-    .then(function(data){
-      console.log(data);
+    .then(function(resp){
+        $scope.data = resp.data || []
+        
+      console.log(resp.data);
     }, function (err) {
         console.log(err)
     })
