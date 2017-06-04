b807a8baee1d71409196f42e73b8300d92b4bad1
Rahul Reddy <rahuldean@gmail.com>
2017-06-04 18:14:40 +0530

    added country filter

--- assets/js/indexCtrl.js
+++ assets/js/indexCtrl.js
@@ -6,11 +6,24 @@ angular.module('bbApp')
     $http.get('assets/raw/data.json')
     .then(function(resp){
         $scope.data = resp.data || []
-        $scope.carrier = $scope.data.carriers[0]
+        $scope.country = $scope.data.countries_states[0]
         $scope.state = $scope.data.countries_states[0].states[0]
+        $scope.carrier = $scope.data.carriers[0]
+        
     }, function (err) {
         console.log(err)
     })
+
+    $scope.countryChanged = function() {
+        $scope.data.countries_states.filter(function(val) {
+            return val.country_id === $scope.country.country_id
+        })
+        $scope.state = $scope.data.countries_states.find(x=> x.country_id === $scope.country.country_id).states[0]
+    }
+
+    $scope.getFilteredCountriesStates = function (countries_states) {
+        return countries_states.country_id == $scope.country.country_id
+    }
 
     $scope.codeFilter = function (code) {
         return $.inArray(parseInt($scope.carrier.carrier_id), code.carriers) > -1 &&
