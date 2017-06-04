997fd2cf9264d049f825c422a46a76b5575e8a05
Rahul Reddy <rahuldean@gmail.com>
2017-06-04 15:07:12 +0530

    integrating filtering of carrier, states

--- assets/js/indexCtrl.js
+++ assets/js/indexCtrl.js
@@ -6,9 +6,14 @@ angular.module('bbApp')
     $http.get('assets/raw/data.json')
     .then(function(resp){
         $scope.data = resp.data || []
-        
-      console.log(resp.data);
+        $scope.carrier = $scope.data.carriers[0]
+        $scope.state = $scope.data.countries_states[0].states[0]
     }, function (err) {
         console.log(err)
     })
+
+    $scope.codeFilter = function (code) {
+        return $.inArray(parseInt($scope.carrier.carrier_id), code.carriers) > -1 &&
+                $.inArray(parseInt($scope.state.state_id), code.states) > -1 
+    }
 }])
